8cf505518f27d814b62153e4137b5e376f9c0681
CCE-Admin <mzyadat@uba.com.jo>
2016-01-21 15:37:10 +0200

    with abbadi

--- Desktop/java/GenericElectricalEnergyCostCalculator/app/src/main/java/com/veripark/genericelectricalenergycostcalculator/MainActivity.java
+++ Desktop/java/GenericElectricalEnergyCostCalculator/app/src/main/java/com/veripark/genericelectricalenergycostcalculator/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        test from web
+        //test with abbadi
         setContentView(android.R.layout.activity_list_item);
     }
 
