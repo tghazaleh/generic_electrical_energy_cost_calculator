555396f0bb3fe67c7a87e9e18f6b6803969eaf30
tghazaleh <tghazaleh@ccecentrix.com>
2016-01-21 15:10:16 +0200

    Update MainActivity.java

--- Desktop/java/GenericElectricalEnergyCostCalculator/app/src/main/java/com/veripark/genericelectricalenergycostcalculator/MainActivity.java
+++ Desktop/java/GenericElectricalEnergyCostCalculator/app/src/main/java/com/veripark/genericelectricalenergycostcalculator/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+//        test from web
         setContentView(android.R.layout.activity_list_item);
     }
 
