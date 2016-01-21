023cf1c32a12cab35f583a2a56954ad8862724ea
CCE-Admin <mzyadat@uba.com.jo>
2016-01-21 15:08:40 +0200

    all the project

---
+++ Desktop/java/GenericElectricalEnergyCostCalculator/app/src/main/java/com/veripark/genericelectricalenergycostcalculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.veripark.genericelectricalenergycostcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+//import com.veripark.genericelectricalenergycostcalculator.R;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(android.R.layout.activity_list_item);
+    }
+
+
+
+}
